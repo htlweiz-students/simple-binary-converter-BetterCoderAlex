8788bfc35753f2cda46ff333ed334d722bd29378
Student <foobar@sr.htlweiz.at>
2025-11-27 21:11:49 +0100

    it works, wp03 finished

--- binary_converter/binary_conv/Main.cs
+++ binary_converter/binary_conv/Main.cs
@@ -3,13 +3,8 @@ using binary_conv;
 namespace programm00{
     public class Binary_Main{
         public static int Main(string[] args){
-            Converter number = new Converter("1100");
-            if (Converter.check_if_correct_input()){
-                System.Console.WriteLine("Is binary");
-            }
-            else{
-                System.Console.WriteLine("isnt binary");
-            }
+            Converter binary_to_decimal = new();
+            binary_to_decimal.convert_to_decimal();
             return 0;
         }
     }
--- binary_converter/binary_conv/Program.cs
+++ binary_converter/binary_conv/Program.cs
@@ -1,20 +1,35 @@
-﻿using System.Numerics;
+﻿
+namespace binary_conv
+{
+    public class Converter
+    {
+        bool is_it_binary = true;
+        string read_console;
+        int count = 0;
 
-namespace binary_conv{
-    public class Converter{
-        private string[] possible_input ={"0", "1"};
-        private string Binary_input;
-        public Converter(string binary_number){
-            Binary_input = binary_number;
-        }
+        public void convert_to_decimal()
+        {
+            int total = 0;
 
-        public void check_if_correct_input(){
-            for (int count = 0; count<Binary_input.Length; count++){
-                if (Binary_input != "1"|| Binary_input != "0"){
-                    Console.WriteLine($"Error: incorrect input in {Binary_input}");
+            while (is_it_binary == true)
+            {
+                Console.WriteLine($"binary number {count} (bit {count}): ");
+                read_console = Console.ReadLine();
+
+                if (read_console == "0" || read_console == "1")
+                {
+                    total = total * 2;
+                    if (read_console == "1")
+                        total++;
+
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine($"Decimal value: {total}");
+                    is_it_binary = false;
                 }
             }
         }
-
     }
 }
