9f044df647d4cc00f59d084ec7a2fe10850d1b1c
Student <foobar@sr.htlweiz.at>
2025-12-01 12:37:19 +0100

    made a decimal to binary converter wp03 extended

--- Programm00/Program.cs
+++ Programm00/Program.cs
@@ -1,10 +1,13 @@
-using binary_conv;
+﻿using binary_converter;
 
 namespace programm00{
     public class Binary_Main{
         public static int Main(string[] args){
             Converter binary_to_decimal = new();
-            binary_to_decimal.convert_to_decimal();
+            binary_to_decimal.Convert_to_decimal();
+
+            Converter decimal_to_binary = new();
+            decimal_to_binary.Convert_to_binary();
             return 0;
         }
     }
---
+++ binary_converter/Class1.cs
@@ -0,0 +1,71 @@
+﻿namespace binary_converter
+{
+    public class Converter
+    {
+        string? read_console;
+        int count = 0;
+
+        public void Convert_to_decimal()
+        {
+            int total = 0;
+
+            while (true)
+            {
+                Console.WriteLine($"binary number {count + 1} (bit {count + 1}): ");
+                read_console = Console.ReadLine();
+
+                if (read_console == "0" || read_console == "1")
+                {
+                    total = total * 2;
+                    if (read_console == "1")
+                        total++;
+
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine($"Decimal value: {total}");
+                    break;
+                }
+            }
+        }
+        public void Convert_to_binary()
+        {
+            int input;
+            string[] arr = [];
+            System.Console.WriteLine("Enter a decimal number:");
+            read_console = Console.ReadLine();
+            if (!int.TryParse(read_console, out input))
+            {
+                System.Console.WriteLine("Invalid input");
+            }
+            int.TryParse(read_console, out input);
+            if (input == 0)
+            {
+                System.Console.WriteLine("Binary number: 0");
+            }
+
+
+            while (input > 0)
+            {
+                if (input % 2 == 0)
+                {
+                    arr = arr.Append("0").ToArray();
+                }
+                else
+                {
+                    arr = arr.Append("1").ToArray();
+                }
+
+                input = input / 2;
+            }
+            Array.Reverse(arr);
+            System.Console.WriteLine("Binary number:");
+            foreach (var bit in arr)
+            {
+                System.Console.Write(bit);
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
